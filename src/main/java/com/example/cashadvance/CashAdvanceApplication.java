1469a45d772f1c2154fa514aa3b28d2e71827eeb
Gangadhar5168 <prathapvg5168@gmail.com>
2025-09-18 15:04:01 +0530

    initial commit - structure created

---
+++ src/main/java/com/example/cashadvance/CashAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashAdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cashadvance/CashAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
